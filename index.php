bb2e17c197ba55ee0a0463626fed37a800fcc933
iudizm <iudi@lepidus.com.br>
2024-01-16 18:03:03 -0300

    Updates php-docs of index.php
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#646
    
    Signed-off-by: iudi <iudi@lepidus.com.br>
    Signed-off-by: jhon <jhon@lepidus.com.br>

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * @defgroup plugin_reports_reviewersControlReport
+ * @defgroup plugins_generic_deleteIncompleteSubmissions
  */
 
 /**
- * @file plugins/reports/DeleteIncompleteSubmissions/index.php
+ * @file plugins/generic/deleteIncompleteSubmissions/index.php
  *
  * Copyright (c) 2024 Lepidus Tecnologia
  * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
- * @ingroup plugin_reports_reviewersControlReport
- * @brief Wrapper for Reviewers control report plugin.
+ * @ingroup plugins_generic_deleteIncompleteSubmissions
+ * @brief Wrapper for Delete incomplete submissions generic plugin.
  *
  */
 
